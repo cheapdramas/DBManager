51eca3b1fb1810e65cf658f3d44ceef3344b6d87
rostislav <115248406+cheapdramas@users.noreply.github.com>
2024-12-02 09:45:16 +0200

    Delete database_manager directory

--- database_manager/user_service/main.py
+++
@@ -1,9 +0,0 @@
-from fastapi import FastAPI
-from routes import MainRouter
-
-app = FastAPI(title='User Service')
-app.include_router(MainRouter)
-
-
-
-
--- database_manager/user_service/routes/__init__.py
+++
@@ -1,6 +0,0 @@
-from routes.read import router as read_router
-from fastapi import APIRouter
-
-MainRouter = APIRouter(prefix='/user')
-MainRouter.include_router(read_router)
-
