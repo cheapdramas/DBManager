72942bc0e4b689144403a7d1fee996d20a98669e
rostislav <bileckijrostislav46@gmail.com>
2024-12-08 02:15:57 +0200

    new token config

--- user_service/config.py
+++ user_service/config.py
@@ -1,6 +1,8 @@
 import dotenv
-
+import pathlib
+from datetime import timedelta
 ENV:dict = dotenv.dotenv_values('.env')
+
 
 class DatabaseConfig:
 	user     =    ENV['DATABASE_USER']
@@ -9,3 +11,9 @@ class DatabaseConfig:
 	host     =    ENV['DATABASE_HOST']
 	port     =    ENV['DATABASE_PORT']
 	DSN      =    f'postgresql://{host}/{database}?user={user}&password={password}'
+
+class TokenConfig:
+	private_key = (pathlib.Path(__file__).parent / 'certs' /'private.pem').read_text()
+	public_key = (pathlib.Path(__file__).parent / 'certs' /'public.pem').read_text()
+	expire_min= timedelta(minutes=1)  #3
+	algorithm = 'RS256'
