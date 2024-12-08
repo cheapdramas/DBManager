bacb543065b2ec20a12777860a133e9db307d95d
rostislav <bileckijrostislav46@gmail.com>
2024-12-08 02:13:44 +0200

    login/waits for test

--- user_service/routes/post.py
+++ user_service/routes/post.py
@@ -1,9 +1,12 @@
-import psycopg
 import bcrypt
+import psycopg
 from fastapi import APIRouter,HTTPException
+from psycopg.rows import dict_row
 from models.user import UserRegister,User,UserLogin
-from config import DatabaseConfig
-
+from models.tokens import AccessTokenPayload
+from models.responses import TokenInfo
+from config import DatabaseConfig,TokenConfig
+from auth import generate_token
 
 router = APIRouter()
 
@@ -17,10 +20,11 @@ async def register_user_route(user_info:UserRegister):
 	except Exception as e:
 		raise HTTPException(400,detail=str(e))
 		
-@router.post('/login')
+@router.post('/login',response_model=TokenInfo)
 async def login_user_route(login_info:UserLogin):
-	pass
-
+	
+	access_token = await RouteHelpersFuncs().login_user(login_info)
+	return TokenInfo(access_token=access_token)
 
 
 
@@ -63,26 +67,37 @@ class RouteHelpersFuncs():
 			await conn.close()
 	#NOT FINISHED
 	@staticmethod
-	async def login_user(login_info:UserLogin):
-		async with await RouteHelpersFuncs().connect_to_db() as conn:
-			sql_query_pw = """
-				SELECT password FROM users WHERE login=%s
-			"""
+	async def login_user(login_info:UserLogin) ->str:
+		async with await RouteHelpersFuncs().connect_to_db(row_factory=dict_row) as conn:
+			sql_query = """
+				SELECT * FROM users WHERE login = %s
+			"""			
+			sql_parameters = (login_info.login,)
 			cursor = conn.cursor()
-			await cursor.execute(sql_query_pw,params=(login_info.login))
-			pw_from_db=await cursor.fetchone()
-			pw_from_db:bytes =pw_from_db[0]
-			if pw_from_db == None:
-				raise HTTPException(status_code=404,detail='User not found')
+
+			await cursor.execute(
+				query=sql_query,
+				params=sql_parameters
+			)
+			user_info_fetch:dict = await cursor.fetchone()
+			if user_info_fetch == None:
+				raise HTTPException(status_code=404,detail='User not found(null result)')
+			pw_from_db:bytes = user_info_fetch['password']
+
 			if not bcrypt.checkpw(login_info.password.encode(),pw_from_db):
 				raise HTTPException(status_code=404,detail='User not found(wrong password)')
-
 			
+			access_token = generate_token(
+				payload =AccessTokenPayload(user_id = user_info_fetch['id'],**user_info_fetch).model_dump(),
+			)	
 
 
-			await cursor.close
-			await conn.close
-		
+
+
+			await cursor.close()
+			await conn.close()
+
+			return access_token
 
 
 
