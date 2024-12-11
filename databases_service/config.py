1f1ad81be0cc1576facccd001d8494cf57b84b71
rostislav <bileckijrostislav46@gmail.com>
2024-12-11 09:11:43 +0200

    DBCluster;Token

--- databases_service/config.py
+++ databases_service/config.py
@@ -1,5 +1,5 @@
 import dotenv
-
+import pathlib
 ENV:dict = dotenv.dotenv_values('.env')
 
 
@@ -10,3 +10,14 @@ class MainDBConfig:
 	host     =    ENV['DATABASE_HOST']
 	port     =    ENV['DATABASE_PORT']
 	DSN      =    f'postgresql://{host}/{database}?user={user}&password={password}'
+
+class DBClusterConfig:
+	user     =    ENV['DATABASE_USER']
+	password =    ENV['DATABASE_PASSWORD']
+	database =    'postgres' #add to env
+	host     =    ENV['DATABASE_HOST']
+	port     =    ENV['DATABASE_PORT']
+	DSN      =    f'postgresql://{host}/{database}?user={user}&password={password}'
+
+class TokenConfig:
+	public_key = (pathlib.Path(__file__).parent / 'certs' /'public.pem').read_text
